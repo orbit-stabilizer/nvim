16182efbb4562cbf8b8acf3c61f598fd3b3a984d
orbit-stabilizer <deowra.kamal@gmail.com>
2022-06-08 05:25:31 -0400

    Add toggleterm

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,6 +91,9 @@ return packer.startup(function(use)
   use 'feline-nvim/feline.nvim'
   use 'SmiteshP/nvim-gps'
 
+  -- Terminal
+  use "akinsho/toggleterm.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
